3af740909b67028210bacb1763cae6b888378717
Kyler Deggs <deggskyler@icloud.com>
2021-11-06 19:07:54 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.9.1
+
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+EXPOSE 5000
+
+ENTRYPOINT [ "python" ]
+
+CMD [ "app.py" ]
---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+app.config['SECRET_KEY'] = os.urandom(32)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run()
