675eb2b7d27642a34e9b89ad66914036cf957dea
Kyler Deggs <deggskyler@icloud.com>
2021-11-08 22:12:47 -0600

    Added SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = os.urandom(32)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URI']
+db = SQLAlchemy(app)
+
+
+class Genre(db.Model):
+    genre_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    type = db.Column(db.String(), nullable=False)
+
+
+class Movies(db.Model):
+    movie_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    genre = db.Column(db.Integer, db.ForeignKey('genre.genre_id'), nullable=False)
+    votes = db.Column(db.Integer, default=0, nullable=False)
+
+
+class Reviews(db.Model):
+    review_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    movie = db.Column(db.Integer, db.ForeignKey('movies.movie_id'), nullable=False)
+    reviewer_name = db.Column(db.String, nullable=False)
+    review = db.Column(db.String, nullable=False)
 
 
 @app.route('/')
